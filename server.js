f0d564004c1df3268371090d214140de56219738
paliodellatorre <paliodellatorre@gmail.com>
2026-04-15 19:13:36 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,6 @@
+PORT=3000
+DATABASE_URL=postgresql://postgres:password@localhost:5432/palio_della_torre
+SESSION_SECRET=cambia-questa-chiave
+ADMIN_USERNAME=admin
+ADMIN_PASSWORD=admin123
+NODE_ENV=development
